9a06dc60436233212d8de29af273ae5b12c492e7
Nico <nico.studt@student.uni-luebeck.de>
2022-02-03 13:44:16 +0100

    Viz Fragment
    Added navigation graph
    Added first icons
    Changed colors for theme #TODO

--- app/src/main/java/com/schneewittchen/ros2_mobile/MainActivity.java
+++ app/src/main/java/com/schneewittchen/ros2_mobile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.schneewittchen.ros2_mobile;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
 
 import android.os.Bundle;
 
@@ -10,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     public MainActivity() {
         super(R.layout.activity_main);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
